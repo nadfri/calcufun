b5d85ec5bab453946a71b6dfc68b41dba584d66b
Nader Frigui <nadfri@gmail.com>
2024-11-20 19:11:43 +0100

    First commit

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import PWABadge from '@components/PWABadge/PWABadge';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <PWABadge />
+    </div>
+  );
+}
+
+export default App;
