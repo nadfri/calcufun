5579ffefcf827c2b59fbda813d49edc805b18423
Nader Frigui <nadfri@gmail.com>
2024-11-21 19:15:57 +0100

    Config: add SVG module declaration, update Vite config for SVGR

---
+++ src/types/svg.d.ts
@@ -0,0 +1,5 @@
+declare module '*.svg?react' {
+  import React from 'react'
+  const SVGComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default SVGComponent;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,55 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    svgr(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'calcufun',
-      short_name: 'calcufun',
-      description: 'CalcuFun is an educational and fun app designed to develop mental math skills for children.',
-      theme_color: '#77DD77',
-    },
+      manifest: {
+        name: 'calcufun',
+        short_name: 'calcufun',
+        description:
+          'CalcuFun is an educational and fun app designed to develop mental math skills for children.',
+        theme_color: '#77DD77',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@components': '/src/components',
+      '@assets': '/src/assets',
     },
   },
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+      },
+    },
+  },
+});
