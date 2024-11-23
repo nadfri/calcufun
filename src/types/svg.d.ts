be2a9553f46c6549d1487972b019034dcfaa2378
Nader Frigui <nadfri@gmail.com>
2024-11-23 15:15:01 +0100

    Fix: config eslint

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -1,5 +1,5 @@
 declare module '*.svg?react' {
-  import React from 'react'
+  import React from 'react';
   const SVGComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default SVGComponent;
 }
