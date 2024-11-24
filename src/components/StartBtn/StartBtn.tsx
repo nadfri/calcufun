d68a2d70fc05950bb0a1f3d6613181544202a370
Nader Frigui <nadfri@gmail.com>
2024-11-24 17:53:02 +0100

    Fix: Animations rotate

--- src/components/StartBtn/StartBtn.tsx
+++ src/components/StartBtn/StartBtn.tsx
@@ -8,9 +8,9 @@ export default function StartBtn() {
     <button className="StartBtn" onClick={() => setOpenGame(true)}>
       <span className="red">S</span>
       <span className="cyan">T</span>
-      <span className="yellow rotate10">A</span>
+      <span className="yellow rotate15">A</span>
       <span className="cyan">R</span>
-      <span className="green rotate25">T</span>
+      <span className="green rotate30">T</span>
     </button>
   );
 }
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -6,9 +6,9 @@ export default function Title() {
       <span className="palegreen rotate15">C</span>
       <span className="palegreen">alc</span>
       <span className="palegreen rotate15">u</span>
-      <span className="blue rotate25">F</span>
+      <span className="blue rotate30">F</span>
       <span className="blue">un </span>
-      <span className="blue rotate25">!</span>
+      <span className="blue rotate30">!</span>
     </h1>
   );
 }
