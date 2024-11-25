9c2e238fcc2b48c11e80358f3fb6e56bf8dcc530
Nader Frigui <nadfri@gmail.com>
2024-11-25 11:57:00 +0100

    Fix: animation forwards BalloonList

--- src/components/BallloonList/BalloonList.tsx
+++ src/components/BallloonList/BalloonList.tsx
@@ -12,20 +12,16 @@ export function BalloonList() {
     const current = ref.current;
 
     if (current) {
-      if (isGameOver) {
-        current.style.animationPlayState = 'paused';
-      } else {
-        current.style.animationPlayState = 'running';
-      }
+      if (isGameOver) current.style.animationPlayState = 'paused';
 
       current.style.animation = 'none';
-      void current.offsetWidth; /*Force Reflow CSS*/
-      current.style.animation = `float-up ${DURATION}ms linear`;
+      void current.offsetWidth; /*Force Reflow Animation CSS*/
+      current.style.animation = `float-up ${DURATION}ms linear forwards`;
     }
   }, [isGameOver, currentTable.key]);
 
   return (
-    <div className="BalloonList" style={{ animationDuration: `${DURATION}ms` }} ref={ref}>
+    <div className="BalloonList" ref={ref}>
       {currentTable.solutions.map((solution, index) => (
         <BalloonNumber key={index + currentTable.key} index={index} solution={solution} />
       ))}
