11209c91b11df7e89d13fc401701f36b747a9969
Nader Frigui <nadfri@gmail.com>
2024-11-26 15:42:21 +0100

    Refacto: Balloons & animations

--- src/components/BalloonNumber/BalloonNumber.tsx
+++ src/components/BalloonNumber/BalloonNumber.tsx
@@ -3,34 +3,9 @@ import { useState } from 'react';
 import { useAudio } from '@hooks/useAudio';
 import { useStoreGame } from '@store/useStoreGame';
 
-import Balloon0 from '@assets/icons/balloons/balloon-0.svg?react';
-import Balloon1 from '@assets/icons/balloons/balloon-1.svg?react';
-import Balloon2 from '@assets/icons/balloons/balloon-2.svg?react';
-import Balloon3 from '@assets/icons/balloons/balloon-3.svg?react';
-import Balloon4 from '@assets/icons/balloons/balloon-4.svg?react';
-import Balloon5 from '@assets/icons/balloons/balloon-5.svg?react';
-import Balloon6 from '@assets/icons/balloons/balloon-6.svg?react';
-import Balloon7 from '@assets/icons/balloons/balloon-7.svg?react';
-import Balloon8 from '@assets/icons/balloons/balloon-8.svg?react';
-import Balloon9 from '@assets/icons/balloons/balloon-9.svg?react';
-import Balloon10 from '@assets/icons/balloons/balloon-10.svg?react';
-
 import explosionURL from '@assets/sounds/explosion.mp3';
 import ConfettiExplosion from 'react-confetti-explosion';
-
-const balloons: Record<number, JSX.Element> = {
-  0: <Balloon0 className="balloon" />,
-  1: <Balloon1 className="balloon" />,
-  2: <Balloon2 className="balloon" />,
-  3: <Balloon3 className="balloon" />,
-  4: <Balloon4 className="balloon" />,
-  5: <Balloon5 className="balloon" />,
-  6: <Balloon6 className="balloon" />,
-  7: <Balloon7 className="balloon" />,
-  8: <Balloon8 className="balloon" />,
-  9: <Balloon9 className="balloon" />,
-  10: <Balloon10 className="balloon" />,
-};
+import { balloonIcons } from './balloonIcons';
 
 type Props = {
   solution: number;
@@ -50,7 +25,7 @@ export function BalloonNumber({ solution, index }: Props) {
 
   const explosionAudio = useAudio(explosionURL);
 
-  const BalloonIcon = balloons[index];
+  const BalloonIcon = balloonIcons[index];
 
   const handleClick = () => {
     const currentSolution = currentTable.tableOf * currentTable.randomNumbers[count];
@@ -70,11 +45,8 @@ export function BalloonNumber({ solution, index }: Props) {
     );
 
   return (
-    <button
-      onClick={handleClick}
-      className={index % 2 === 0 ? 'BalloonNumber zigzag' : 'BalloonNumber zigzag2'}
-    >
-      {BalloonIcon ? BalloonIcon : <Balloon1 className="balloon" />}
+    <button onClick={handleClick} className={`BalloonNumber zigzag${index}`}>
+      <BalloonIcon className="balloonIcon" />
 
       <div className="solution">{solution}</div>
     </button>
---
+++ src/components/BalloonNumber/balloonIcons.ts
@@ -0,0 +1,28 @@
+import Balloon0 from '@assets/icons/balloons/balloon-0.svg?react';
+import Balloon1 from '@assets/icons/balloons/balloon-1.svg?react';
+import Balloon2 from '@assets/icons/balloons/balloon-2.svg?react';
+import Balloon3 from '@assets/icons/balloons/balloon-3.svg?react';
+import Balloon4 from '@assets/icons/balloons/balloon-4.svg?react';
+import Balloon5 from '@assets/icons/balloons/balloon-5.svg?react';
+import Balloon6 from '@assets/icons/balloons/balloon-6.svg?react';
+import Balloon7 from '@assets/icons/balloons/balloon-7.svg?react';
+import Balloon8 from '@assets/icons/balloons/balloon-8.svg?react';
+import Balloon9 from '@assets/icons/balloons/balloon-9.svg?react';
+import Balloon10 from '@assets/icons/balloons/balloon-10.svg?react';
+
+export const balloonIcons: Record<
+  number,
+  React.ComponentType<React.SVGProps<SVGSVGElement>>
+> = {
+  0: Balloon0,
+  1: Balloon1,
+  2: Balloon2,
+  3: Balloon3,
+  4: Balloon4,
+  5: Balloon5,
+  6: Balloon6,
+  7: Balloon7,
+  8: Balloon8,
+  9: Balloon9,
+  10: Balloon10,
+};
